d4df2dd960174d8ea1d206b820065e3aa083283c
Bartek <bartek.polomski@gmail.com>
2018-07-26 20:12:13 +0200

    [SA-2] Intialize Spring Project

---
+++ src/main/java/com/demo/spring/errorhandler/ErrorHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.demo.spring.errorhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorHandlerApplication.class, args);
+	}
+}
